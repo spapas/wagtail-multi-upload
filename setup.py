08f05e64741c05383b044eff8684d2329d8cfd45
Serafeim Papastefanos <spapas@gmail.com>
2022-02-14 20:54:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtail-multi-upload',
-    version='0.0.5',
+    version='0.0.6',
     description='',
 
     author='Serafeim Papastefanos',
